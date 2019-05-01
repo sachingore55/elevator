3880d36a3674ef6f9aaf6bf5e3d2c51ea19fbbf9
sachingore55 <sachingore55@gmail.com>
2019-05-01 15:20:03 +0530

    Add files via upload
    Improved code quality and passed all 3 arguments (floor,direction,destination_floor) in single function call

--- elevator.py
+++ elevator.py
@@ -1,63 +1,51 @@
 from time import sleep
 
-CURRENT_ELEVATOR_POSITION=1
+CURRENT_ELEVATOR_POSITION=0
 
-class Elevator():
+class Elevator(object):
 
-    def call_Elevator(self,floor,direction):
+    def call_Elevator(self,floor,direction,destination_floor):
                  global CURRENT_ELEVATOR_POSITION
                  if direction==1:
                     print("Called Elevator at floor %i for going up.. " %floor)
+                    self.move_elevator(floor,direction,destination_floor)
+                    #self.select_floor(direction,destination_floor)
 
-                    for count in range (CURRENT_ELEVATOR_POSITION-1,floor+1):
-                        current_floor = count
-                        print("Elevator is moving up and is at floor %i ..." %count)
-                        sleep(2)
-                    CURRENT_ELEVATOR_POSITION = current_floor
-                    print("Elevator is currently at floor : %i" %CURRENT_ELEVATOR_POSITION)
-
-
-                 elif direction==-1:
+                 else:
                     print("Called Elevator at floor %i for going down.. " %floor)
                     print("Elevator is currently at floor : %i" %CURRENT_ELEVATOR_POSITION)
+                    self.move_elevator(floor,direction,destination_floor)
 
-                    for count in range (CURRENT_ELEVATOR_POSITION-1,floor-1,-1):
-                        current_floor = count
-                        print("Elevator is moving down and is at floor %i ..." %count)
-                        sleep(2)
-                    CURRENT_ELEVATOR_POSITION = current_floor
-                    print("Elevator is currently at floor : %i" %CURRENT_ELEVATOR_POSITION)
-
-
-    def select_Floor(self,destination_floor):
-
+    def move_elevator(self,floor,direction,destination_floor):
         global CURRENT_ELEVATOR_POSITION
         print("Current elevator position => %i" % CURRENT_ELEVATOR_POSITION)
-        print("Passenger has boarded the elevator and selected destination floor as %i" %destination_floor)
+        current_floor=CURRENT_ELEVATOR_POSITION
 
-        if CURRENT_ELEVATOR_POSITION < destination_floor:
-            for count in range (CURRENT_ELEVATOR_POSITION+1,destination_floor+1):
-                current_floor = count
-                print("Elevator is moving UP and is at floor %i ..." %count)
+        while(current_floor != floor):
+            if(floor > current_floor):
+                current_floor += 1
+                print("Elevator is at floor %i ..." % current_floor)
                 sleep(2)
-            CURRENT_ELEVATOR_POSITION = current_floor
-            print("Person has alighted at floor %i" %CURRENT_ELEVATOR_POSITION)
+            else:
+                current_floor -= 1
+                print("Elevator is at floor %i ..." % current_floor)
+                sleep(2)
+        CURRENT_ELEVATOR_POSITION = current_floor
+        print("Elevator has arrived at floor : %i" % CURRENT_ELEVATOR_POSITION)
 
-        elif CURRENT_ELEVATOR_POSITION > destination_floor:
-            for count in range (CURRENT_ELEVATOR_POSITION-1,destination_floor-1,-1):
-                current_floor = count
-                print("Elevator is moving DOWN and is at floor %i ..." %count)
-                sleep(2)
-            CURRENT_ELEVATOR_POSITION = current_floor
-            print("Person has alighted at floor %i" %CURRENT_ELEVATOR_POSITION)
+        print("Passenger has selected destination floor as : %i" % destination_floor)
+        while (current_floor != destination_floor):
+            current_floor += direction
+            print("Elevator is at floor %i ..." % current_floor)
+            sleep(2)
+        CURRENT_ELEVATOR_POSITION = current_floor
+        print("Passenger has alighted at floor : %i" % CURRENT_ELEVATOR_POSITION)
 
 
 elv = Elevator()
-elv.call_Elevator(5,1)
-elv.select_Floor(9)
-elv.call_Elevator(7,-1)
-elv.select_Floor(3)
-elv.call_Elevator(2,1)
-elv.select_Floor(9)
-#elv.call_Elevator(1,1)
+elv.call_Elevator(6,1,9)
+elv.call_Elevator(4,-1,1)
+#elv.call_Elevator(3,1,8)
+# elv.call_Elevator(3,1,10)
+# elv.call_Elevator(7,-1,0)
 #elv.select_Floor(7)
